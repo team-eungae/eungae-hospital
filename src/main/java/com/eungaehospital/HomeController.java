6b5dd464e61fc0773e88ab113cd08733a9669cf9
sooyong <hwasuyong99@naver.com>
2024-01-18 14:00:58 +0900

    [ALL] Feat: 로그인 성공시 페이지 이동

--- src/main/java/com/eungaehospital/HomeController.java
+++ src/main/java/com/eungaehospital/HomeController.java
@@ -1,6 +1,7 @@
 package com.eungaehospital;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -9,9 +10,14 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class HomeController {
 
-	@GetMapping
+	@GetMapping("/")
 	public String getLoginForm() {
 		return "index";
 	}
 
+	@GetMapping("/login/error")
+	public String loginError(Model model) {
+		model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+		return "index";
+	}
 }
