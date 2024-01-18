1111978684b52c16834100adeb53e486f3bd90e3
sooyong <hwasuyong99@naver.com>
2024-01-18 09:26:11 +0900

    initial

---
+++ src/main/java/com/eungaehospital/EungaeHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.eungaehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EungaeHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EungaeHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eungaehospital/EungaeHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eungaehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EungaeHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
