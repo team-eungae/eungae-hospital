dcdf64bbf64753e5229e83354a5ef79476557261
jihye <jihye07201@gmail.com>
2024-01-22 18:37:41 +0900

    [ALL] Feat: 병원 상세보기 기능 구현
    Feat: 병원 상세보기 기능 구현
    This closes #24

--- src/main/java/com/eungaehospital/hospital/controller/HospitalViewController.java
+++ src/main/java/com/eungaehospital/hospital/controller/HospitalViewController.java
@@ -2,12 +2,17 @@ package com.eungaehospital.hospital.controller;
 
 import com.eungaehospital.doctor.dto.DoctorResponseDto;
 import com.eungaehospital.doctor.service.DoctorService;
+import com.eungaehospital.hospital.dto.HospitalUpdateRequestDto;
+import com.eungaehospital.hospital.dto.HospitalViewResponseDto;
+import com.eungaehospital.hospital.service.HospitalService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,7 +20,7 @@ import java.util.List;
 @RequestMapping("/hospital")
 @Controller
 public class HospitalViewController {
-
+	private final HospitalService hospitalService;
 	private final DoctorService doctorService;
 
 	@GetMapping("/main")
@@ -27,12 +32,18 @@ public class HospitalViewController {
 	}
 
 	@GetMapping("/profile")
-	public String hospitalPage() {
+	public String hospitalPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		HospitalViewResponseDto hospital = hospitalService.getHospitalByHospitalId(
+			userDetails.getUsername());
+		model.addAttribute("hospital", hospital);
 		return "contents/hospital-detail";
 	}
 
 	@GetMapping("/profile/form")
-	public String updateHospital() {
+	public String updateHospital(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		HospitalViewResponseDto hospital = hospitalService.getHospitalByHospitalId(
+			userDetails.getUsername());
+		model.addAttribute("hospital", hospital);
 		return "contents/hospital-update";
 	}
 
---
+++ src/main/java/com/eungaehospital/hospital/dto/HospitalViewResponseDto.java
@@ -0,0 +1,44 @@
+package com.eungaehospital.hospital.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.eungaehospital.hospital.domain.Hospital;
+import com.eungaehospital.hospital.domain.HospitalImage;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class HospitalViewResponseDto {
+
+	private Long hospitalSeq;
+	private String hospitalId;
+	private String password;
+	private String name;
+	private String notice;
+	private int deposit; //예약금
+	private String contact; //연락처
+	private String address;
+	private List<HospitalImageResponseDto> hospitalImageResponseDtoList;
+
+	public static HospitalViewResponseDto toDto(Hospital entity, List<HospitalImage> hospitalImageList) {
+
+		List<HospitalImageResponseDto> hospitalImageDtoList = hospitalImageList.stream()
+			.map(HospitalImageResponseDto::toDto)
+			.collect(Collectors.toList());
+
+		return HospitalViewResponseDto.builder()
+			.hospitalSeq(entity.getHospitalSeq())
+			.hospitalId(entity.getHospitalId())
+			.password(entity.getPassword())
+			.name(entity.getName())
+			.notice(entity.getNotice())
+			.deposit(entity.getDeposit())
+			.contact(entity.getContact())
+			.address(entity.getAddress())
+			.hospitalImageResponseDtoList(hospitalImageDtoList)
+			.build();
+	}
+}
