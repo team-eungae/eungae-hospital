0be382f1cc2697a0bf65a38238900c000cd75cf4
김우진 <ooojin523@gmail.com>
2024-01-22 12:45:54 +0900

    [BE] Fix: 의사 출근 상태 수정
    의사 출근 상태 수정
    This closes #20

--- src/main/java/com/eungaehospital/doctor/controller/DoctorViewController.java
+++ src/main/java/com/eungaehospital/doctor/controller/DoctorViewController.java
@@ -4,8 +4,10 @@ import com.eungaehospital.doctor.service.DoctorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
+@RequestMapping("/hospital")
 @Controller
 public class DoctorViewController {
 
