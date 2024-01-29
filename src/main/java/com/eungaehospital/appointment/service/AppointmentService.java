eaf1458ad7a461060253f3fd93617fb9940b2bc0
김우진 <ooojin523@gmail.com>
2024-01-29 17:20:59 +0900

    [ALL] Feat: 현장 등록 기능 구현
    현장 등록 기능 구현했습니다.
    This closes #39

--- src/main/java/com/eungaehospital/appointment/service/AppointmentService.java
+++ src/main/java/com/eungaehospital/appointment/service/AppointmentService.java
@@ -1,63 +1,97 @@
 package com.eungaehospital.appointment.service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
+import com.eungaehospital.appointment.domain.Appointment;
+import com.eungaehospital.appointment.domain.AppointmentStatus;
+import com.eungaehospital.appointment.dto.AppointmentRequestDto;
+import com.eungaehospital.appointment.dto.AppointmentResponseDto;
+import com.eungaehospital.appointment.repository.AppointmentRepository;
+import com.eungaehospital.doctor.domain.Doctor;
+import com.eungaehospital.doctor.repository.DoctorRepository;
+import com.eungaehospital.hospital.domain.Hospital;
+import com.eungaehospital.hospital.repository.HospitalRepository;
+import com.eungaehospital.member.domain.Children;
+import com.eungaehospital.member.domain.Member;
+import com.eungaehospital.member.repository.ChildrenRepository;
+import com.eungaehospital.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.eungaehospital.appointment.domain.Appointment;
-import com.eungaehospital.appointment.domain.AppointmentStatus;
-import com.eungaehospital.appointment.dto.AppointmentResponseDto;
-import com.eungaehospital.appointment.repository.AppointmentRepository;
-import com.eungaehospital.hospital.repository.HospitalRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class AppointmentService {
 
-	private final AppointmentRepository appointmentRepository;
-	private final HospitalRepository hospitalRepository;
+    private final AppointmentRepository appointmentRepository;
+    private final HospitalRepository hospitalRepository;
+    private final DoctorRepository doctorRepository;
+    private final MemberRepository memberRepository;
+    private final ChildrenRepository childrenRepository;
 
-	@Transactional(readOnly = true)
-	public List<AppointmentResponseDto> getAppointmentListBySelectedDate(String hospitalId, String selectDate) {
-		Long hospitalSeq = hospitalRepository.findByHospitalId(hospitalId).get().getHospitalSeq();
 
-		LocalDate appointmentDate = convertStringToLocalDate(selectDate);
-		List<Appointment> appointment = appointmentRepository
-			.getAppointmentByAppointmentDateAndHospitalId(
-				hospitalSeq,
-				appointmentDate);
+    @Transactional(readOnly = true)
+    public List<AppointmentResponseDto> getAppointmentListBySelectedDate(String hospitalId, String selectDate) {
+        Long hospitalSeq = hospitalRepository.findByHospitalId(hospitalId).get().getHospitalSeq();
 
-		appointment.addAll(
-			appointmentRepository.getDiagnosisAppointmentByAppointmentDateAndHospitalId(hospitalSeq, appointmentDate));
+        LocalDate appointmentDate = convertStringToLocalDate(selectDate);
+        List<Appointment> appointment = appointmentRepository
+                .getAppointmentByAppointmentDateAndHospitalId(
+                        hospitalSeq,
+                        appointmentDate);
 
-		return appointment.stream()
-			.map(AppointmentResponseDto::toDto)
-			.toList();
-	}
+        appointment.addAll(
+                appointmentRepository.getDiagnosisAppointmentByAppointmentDateAndHospitalId(hospitalSeq, appointmentDate));
 
-	@Transactional
-	public void changeAppointmentStatus(Long appointmentSeq, String status) {
+        return appointment.stream()
+                .map(AppointmentResponseDto::toDto)
+                .toList();
+    }
 
-		Appointment appointment = appointmentRepository.findById(appointmentSeq)
-			.orElseThrow(() -> new IllegalStateException(
-				"Cannot find Appointment. appointmentSeq = {%d}".formatted(appointmentSeq)));
+    @Transactional
+    public void changeAppointmentStatus(Long appointmentSeq, String status) {
 
-		if (status.equals("visit")) {
-			appointment.setStatus(AppointmentStatus.DIAGNOSIS);
-		} else if (status.equals("restore")) {
-			appointment.setStatus(AppointmentStatus.APPOINTMENT);
-		}
-	}
+        Appointment appointment = appointmentRepository.findById(appointmentSeq)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Cannot find Appointment. appointmentSeq = {%d}".formatted(appointmentSeq)));
 
-	private LocalDate convertStringToLocalDate(String date) {
-		return LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
-	}
+        if (status.equals("visit")) {
+            appointment.setStatus(AppointmentStatus.DIAGNOSIS);
+        } else if (status.equals("restore")) {
+            appointment.setStatus(AppointmentStatus.APPOINTMENT);
+        }
+    }
 
+    private LocalDate convertStringToLocalDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+    }
+
+    @Transactional
+    public void saveAppointment(AppointmentRequestDto requestDto, String hospitalId) {
+        Hospital hospital = hospitalRepository.findByHospitalId(hospitalId)
+                .orElseThrow(
+                        () -> new NoSuchElementException("can not found hospital. hospitalId = {%s}".formatted(hospitalId)));
+
+        Doctor doctor = doctorRepository.findById(requestDto.getDoctorSeq())
+                .orElseThrow(() -> new IllegalStateException(
+                        "Can not found Doctor. doctorSeq = {%d}".formatted(requestDto.getDoctorSeq())));
+
+        Member member = memberRepository.findById(1L).get();
+
+        Children children = Children.builder()
+                .member(member)
+                .name(requestDto.getName())
+                .birthDate(requestDto.getBirth())
+                .gender(requestDto.getGender())
+                .build();
+
+        childrenRepository.save(children);
+        Appointment appointment = AppointmentRequestDto.toEntity(requestDto, doctor, children, member, hospital);
+        appointmentRepository.save(appointment);
+    }
 }
--- src/main/java/com/eungaehospital/doctor/controller/DoctorApiController.java
+++ src/main/java/com/eungaehospital/doctor/controller/DoctorApiController.java
@@ -4,7 +4,6 @@ import com.eungaehospital.doctor.domain.DoctorStatus;
 import com.eungaehospital.doctor.service.DoctorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
